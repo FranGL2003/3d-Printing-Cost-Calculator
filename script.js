98f40c93aba60f102ef1dc3b4838eb7e5aa1058c
FranGL2003 <104400065+FranGL2003@users.noreply.github.com>
2022-11-30 23:36:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+const consumo = document.getElementById('consumo');
+const precioWatt = document.getElementById('precio-watt');
+const duracion = document.getElementById('duracion');
+const peso = document.getElementById("peso");
+const precioGramo = document.getElementById('precio-gr');
+const nombre = document.getElementById('name')
+let resultado = document.getElementById('resultado');
+let itemsList = document.getElementById('items-list');
+let items = []
+let duracionDecimal = 
+
+// Poner los ultimos valores apenas carga la página
+consumo.value = localStorage.getItem('consumo')
+precioWatt.value = localStorage.getItem('precioWatt')
+duracion.value = localStorage.getItem('duracion')
+precioGramo.value = localStorage.getItem('precioGramo')
+peso.value = localStorage.getItem('peso')
+
+items = (JSON.parse(localStorage.getItem('productos')))
+renderItemsList()
+
+//Calcular el costo
+function calculate () {    
+    horaADecimal(duracion.value)
+    
+    resultado.innerText = '$ ' +  Number((consumo.value * precioWatt.value * duracionDecimal + peso.value * precioGramo.value).toFixed(2))  
+
+    localStorage.setItem('consumo', consumo.value)
+    localStorage.setItem('precioWatt', precioWatt.value)
+    localStorage.setItem('duracion', duracion.value)
+    localStorage.setItem('peso', peso.value)
+    localStorage.setItem('precioGramo', precioGramo.value)
+}
+
+//Guardar los productos
+function saveName() {
+    if (nombre.value !== '' && resultado.innerText !== '') {
+        items.push({nombre: nombre.value, precio: resultado.innerText })
+        localStorage.setItem('productos', JSON.stringify(items))
+        renderItemsList()}
+        else {
+            alert('Falta el nombre o el costo')
+        }
+    }
+
+//Renderizar los productos
+function renderItemsList() {
+    itemsList.innerHTML = ' '
+    for (let i = 0; i < items.length; i++) {
+        itemsList.innerHTML += `<li> ${items[i].nombre} / ${items[i].precio} </li>`
+    } 
+}
+
+//Elimiar todos los productos
+function deleteAll() {
+    items = []
+    localStorage.removeItem('productos')
+    renderItemsList()
+}
+
+//Eliminar el último producto
+function deleteLast(item) {
+    items.splice(items.length - 1, 1)
+    localStorage.setItem('productos', JSON.stringify(items))
+    renderItemsList()
+}
+
+//Función para transformar la hora a sistema decimal
+function horaADecimal(hora) {
+    let decimal = hora / 60
+
+    duracionDecimal = decimal
+  }
