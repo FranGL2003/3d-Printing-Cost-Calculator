9b344e2c275edd36960f696d81c173522b5445e6
FranGL2003 <104400065+FranGL2003@users.noreply.github.com>
2022-12-01 12:28:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ duracion.value = localStorage.getItem('duracion')
 precioGramo.value = localStorage.getItem('precioGramo')
 peso.value = localStorage.getItem('peso')
 
-items = (JSON.parse(localStorage.getItem('productos')))
+items = JSON.parse(localStorage.getItem('productos'))
 renderItemsList()
 
 //Calcular el costo
